ad7d5dd680bde67172b705d9c647cb4af1b80b86
nikan-akbardoost-salimi <nikan-akbardoost-salimi@proton.me>
2024-05-11 21:46:31 +0330

    Speech-To-Text

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const recognitionSvc = window?.SpeechRecognition || window?.webkitSpeechRecognition;
+let recognition;
+
+try {
+  recognition = new recognitionSvc();
+} catch(error) {
+  document.getElementById('transcription').innerHTML = `<p>This browser does not support speech recognition API. Please, use another one (Google Chrome recommended)</p>`;
+}
+
+document.querySelector('#start').addEventListener('click', ()=>{
+    const startBtn = document.querySelector('#start');
+    recognition.lang = document.querySelector('#lang').value || 'fa-IR';
+    recognition.continuous = true;
+
+    recognition.onresult = (event) => {
+      const accumulatedResult = [];
+      for (const result of event.results) accumulatedResult.push(`${result[0].transcript}`);
+      let currentText = document.querySelector('#transcription').value;
+      currentText += accumulatedResult.join(' ');
+      document.querySelector('#transcription').value = currentText;
+    };
+
+    if(startBtn.classList.contains('listening')) recognition.stop();
+    else recognition.start();
+
+    startBtn.classList.toggle('listening');
+});
+document.querySelector('.copy').addEventListener('click', () => {
+    const transcriptionText = document.querySelector('#transcription').value;
+    navigator.clipboard.writeText(transcriptionText).then(() => {
+        document.querySelector('#transcription').title = "Copied to clipboard!";
+    }).catch(err => {
+        console.error('Failed to copy text: ', err);
+    });
+});
